45f5f6e630cf7be16cf3e2358fd68c4547099859
majeed1419 <abdulmajeed.mansor@gmail.com>
2024-05-17 22:45:18 +0300

    abdulmajeed

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ with col2:
     st.image('pic.png')
 with col3:
     st.write("")
+st.markdown("Despite the challenges of finding a suitable apartment, there are steps you can take to find one in Riyadh at a reasonable price.Upon receiving a job opportunity in Riyadh, I began searching for suitable accommodation. During my search, I came across a dataset that illustrated the factors influencing the decision to rent.Firstly, we looked into which neighborhoods have the highest demand for rental apartments. Through the dataset, it became clear which neighborhoods are available for rent.")
+
 district_counts = df['district'].value_counts()
 top_10_districts = district_counts.nlargest(10)
 df_top_10 = df[df['district'].isin(top_10_districts.index)]
