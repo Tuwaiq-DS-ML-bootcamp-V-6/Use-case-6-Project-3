76930dc7beadd5112d1d1f46e39fd7afa43f71a9
majeed1419 <abdulmajeed.mansor@gmail.com>
2024-05-17 22:14:19 +0300

    abdulmajeed

--- app.py
+++ app.py
@@ -15,12 +15,7 @@ top_10_districts = district_counts.nlargest(10)
 df_top_10 = df[df['district'].isin(top_10_districts.index)]
 
 # Create the pie chart
-fig = px.pie(df_top_10, 
-             values=df_top_10['district'].value_counts().values, 
-             names=df_top_10['district'].value_counts().index, 
-             title='Top 10 Districts by Number of Properties in Riyadh')
-fig.update_layout(width=800, height=500)
+st.write('Top 10 Districts by Number of Properties in Riyadh')
+st.image("sec_pic.png")
 
-# Display the pie chart in Streamlit
 
-st.plotly_chart(fig)
