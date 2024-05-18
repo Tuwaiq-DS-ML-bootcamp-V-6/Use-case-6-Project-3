0002f00bd616b40f402a6dbe68d17d0478b9bd7c
majeed1419 <abdulmajeed.mansor@gmail.com>
2024-05-18 22:23:03 +0300

    abdulmajeed

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ def load_lottieurl(url: str):
 realestate = load_lottiefile("ani1.json")
 
 st_lottie(realestate,key="real_estate")
-
+st.title("the truth of real estate in Riyadh")
 
 # Calculate the top 10 districts
 df=pd.read_csv("realEstate.csv")
-st.markdown("What factors make the real estate market in Riyadh attractive to investors?The real estate market for apartments in Riyadh is considered one of the most important property markets in the Kingdom of Saudi Arabia for investors. Riyadh offers diverse and lucrative opportunities for investing in apartment properties, whether through purchasing and leasing apartments or developing new real estate projects.During our research, a set of data revealed the most suitable places for investment.Various areas in Riyadh cater to the needs of investors, ranging from upscale residential neighborhoods to industrial and commercial zones. Investors can achieve substantial returns on their investments in Riyadh's apartment market due to the strong demand for housing in the city.")
+st.markdown("The real estate market for apartments in Riyadh is considered one of the most important property markets in the Kingdom of Saudi Arabia for investors. Riyadh offers diverse and lucrative opportunities for investing in apartment properties, whether through purchasing and leasing apartments or developing new real estate projects. Our research has identified the most suitable places for investment, revealing various areas in Riyadh that cater to the needs of investors. These range from upscale residential neighborhoods to industrial and commercial zones. Investors can achieve substantial returns on their investments in Riyadh’s apartment market due to the strong demand for housing in the city.")
 
 district_counts = df['district'].value_counts()
 top_10_districts = district_counts.nlargest(10)
@@ -59,7 +59,7 @@ st.image(framed_image, use_column_width=True)
 
 
 
-st.markdown("From our analysis, we found that an increase in the number of bathrooms is associated with an increase in the apartment size. However, this leads to a significant rise in prices, making them less attractive to buyers and investors. Therefore, investing in moderately sized apartments with a reasonable number of bathrooms can be more effective in attracting buyers and ensuring a quicker return on investment.")
+st.markdown("From our analysis, we found that an increase in the number of bathrooms is associated with an increase in apartment size. However, this leads to a significant rise in prices, making them less attractive to buyers and investors. Therefore, investing in moderately sized apartments with a reasonable number of bathrooms can be more effective in attracting buyers and ensuring a quicker return on investment.")
 
 image_path = 'image.png'
 image = Image.open(image_path)
@@ -69,7 +69,7 @@ framed_image = ImageOps.expand(image, border=frame_width, fill=frame_color)
 st.image(framed_image, use_column_width=True)
 
 
-st.markdown("In Riyadh's real estate market, the presence of kitchens and air conditioning systems holds immense sway over investor interest. These features not only enhance tenant satisfaction but also underscore the property's value and desirability, making it an attractive investment opportunity.")
+st.markdown("In Riyadh’s real estate market, the presence of kitchens and air conditioning systems holds immense sway over investor interest. These features not only enhance tenant satisfaction but also increase the property’s value and desirability, making it an attractive investment opportunity.")
 
 image_path = 's.png'
 image = Image.open(image_path)
@@ -78,6 +78,6 @@ frame_color = (21, 60, 61)
 framed_image = ImageOps.expand(image, border=frame_width, fill=frame_color)
 st.image(framed_image, use_column_width=True)
 
-st.markdown("In conclusion, the real estate market for apartments in Riyadh offers highly promising and attractive investment opportunities. The diversity of the city's neighborhoods, ranging from upscale residential areas to industrial and commercial zones, provides a wide array of investment options that cater to various preferences and strategies. If you are looking for an investment that yields tangible and quick returns, the apartment market in Riyadh is the perfect destination. Don't miss the opportunity to join the thriving momentum in this dynamic and promising market!")
+st.markdown("In conclusion, the real estate market for apartments in Riyadh offers highly promising and attractive investment opportunities. The diversity of the city’s neighborhoods, ranging from upscale residential areas to industrial and commercial zones, provides a wide array of investment options catering to various preferences and strategies. If you are looking for an investment that yields tangible and quick returns, the apartment market in Riyadh is the perfect destination. Don’t miss the opportunity to join the thriving momentum in this dynamic and promising market!")
 
 
