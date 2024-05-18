da472d9439b6772691c1d7e9660f082f873786f2
Riyadh Alghamdi <you@example.com>
2024-05-18 12:42:17 +0300

    Riyadh Alghamdi

--- app/app.py
+++ app/app.py
@@ -1,17 +1,56 @@
+import streamlit as st
+import plotly.express as px
+import body as b
+
+
 def intro():
-    pass
+    st.title("Find Your New Home in Riyadh!")
+    st.write("""
+             When thinking about moving to Riyadh, imagine you're setting out on an exciting journey. Picture yourself guided by an old friend who knows the city well and is ready to share insights about the best places to live, whether it's a cozy apartment, a luxurious villa, or a perfect plot of land. Silly Belly absolutely got you covered with our insights.
+    """)
+
+
+from urllib.request import urlopen
+import json
 
 
 def body():
-    pass
+    # with urlopen(
+    #     "https://raw.githubusercontent.com/Alnasser0/Saudi_Arabia-GeoJSON/master/SAU-geo.json"
+    # ) as response:
+    #     counties = json.load(response)
+    #     fig = px.choropleth(
+    #         geojson=counties,
+    #         range_color=(0, 12),
+    #     )
+    #     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    #     fig.show()
+
+    b.body()
 
 
 def conclusion():
-    pass
+    st.header("THE END")
+
+
+def sidebar(options):
+    with st.form(key="Form1"):
+        with st.sidebar:
+            st.title("Let old Silly Belly help you")
+            st.image("E:/Dev/Work/Tuwaiq/Use-case-6-Project-3/app/image.png")
+
+            options["select_type"] = st.sidebar.radio(
+                "What kind of property are looking for?",
+                ("Silly Belly's story", "Lands", "Apartments", "Villas"),
+            )
 
 
 def main():
-    pass
+    options = {}
+    sidebar(options)
+    intro()
+    body()
+    conclusion()
 
 
 if __name__ == "__main__":
---
+++ app/body.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def body():
+    st.header("The Land of Dreams")
+    st.write("")
+    st.header("The Villa of Comfort")
+    st.write("")
+    st.header("The Cozy Apartment")
+    st.write("")
