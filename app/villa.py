2cd5a1ec2792287d3940d36166c04907d91b2366
Tw-0l <A.Abdullahtw@gmail.com>
2024-05-18 23:16:49 +0300

    Abduallah Altuwayjiri

--- app/villa.py
+++ app/villa.py
@@ -1,23 +1,80 @@
 import streamlit as st
-
+import pandas as pd
+import plotly.express as px
 
 def main():
-    st.markdown("**How many rooms and bathrooms do villas in Riyadh typically have?**")
+    st.markdown("**TEST**")
+
+villas = pd.read_csv('/Users/aabdu/Desktop/DS-ML-bootcamp/Use-case-6-Project-3/Use-case-6-Project-3/cleaned/villas.csv')
+villas.drop(villas[villas['الحي'] == ' '].index, inplace=True)
 
 
-# feel free to edit it
-"""
+st.text('What is the distribution of properties based on their number of rooms (عدد الغرف)?')
 
-How many rooms and bathrooms do villas in Riyadh typically have?
-Input: [Select number of rooms and bathrooms]
-Insight: Villas in Riyadh vary in the number of rooms and bathrooms, catering to different family sizes and needs. Whether you need multiple bedrooms for a large family or a cozier setup, there's something for everyone.
+fig = px.histogram(villas, x="عدد الغرف", title="Distribution of Number of Rooms")
+st.plotly_chart(fig)
 
-What amenities should you expect in a villa?
-Input: [Select desired amenities]
-Insight: Many villas come with essential amenities such as a maid's room, driver's room, annex, garden, swimming pool, basement, garage, and elevator. These features enhance your living experience, providing convenience and luxury.
 
-What is the price range and area of villas in Riyadh?
-Input: [Enter your budget range]
-Insight: Villas in Riyadh have a wide range of prices and sizes, allowing you to find a villa that fits your budget and space requirements. Options are available across different neighborhoods, from large villas with spacious gardens to more modest homes.
+grouped_villas = villas.groupby(['عدد الغرف', 'عدد الحمامات']).size().reset_index(name='count')
 
-"""
+# Creating the Plotly figure
+fig = px.bar(grouped_villas, x='عدد الغرف', y='count', color='عدد الحمامات',
+             title='عدد الغرف و الحمامات في منازل الرياض',
+             labels={'عدد الغرف': 'عدد الغرف', 'count': 'العدد', 'عدد الحمامات': 'عدد الحمامات'})
+
+fig.update_layout(barmode='stack')
+
+# Creating the Streamlit app
+st.title('Visualization of Villas in Riyadh')
+st.plotly_chart(fig)
+
+
+
+
+
+# amenities_counts = villas.iloc[:, 9:-1].sum().reset_index()
+# amenities_counts.columns = ['Amenity', 'Count']
+
+# st.title("Villa Amenities Chart")
+# fig = px.bar(amenities_counts, x='Amenity', y='Count', title="Amenities in Villas")
+# st.plotly_chart(fig)
+
+
+
+amenities_counts = villas.iloc[:, 9:-1].sum().reset_index()
+amenities_counts.columns = ['Amenity', 'Count']
+
+# Plotting Pie Chart
+st.title("Villa Amenities Pie Chart")
+fig = px.pie(amenities_counts, values='Count', names='Amenity', title="Amenities in Villas")
+st.plotly_chart(fig)
+
+
+
+# Plotting Scatter Plot
+
+
+neighborhood_avg_price = villas.groupby('الحي')[['السعر الاجمالي','المساحة']].median().reset_index()
+st.title("Price vs Area of Villas in Riyadh")
+fig = px.scatter(neighborhood_avg_price, x='المساحة', y='السعر الاجمالي', title="Price vs Area of Villas in Riyadh",
+                 labels={'المساحة': 'Area (sq. m)', 'السعر الاجمالي': 'Price (SAR)'},
+                 color='الحي',log_x=True, log_y=True)
+st.plotly_chart(fig)
+
+
+#last C
+neighborhood_avg_price = villas.groupby('الحي')['السعر الاجمالي'].median().reset_index()
+
+neighborhood_avg_price = neighborhood_avg_price.round()
+neighborhood_avg_price = neighborhood_avg_price.sort_values(by='السعر الاجمالي', ascending=True).head(10)
+# Plotting bar chart
+st.title("Average Price of Villas in Each Neighborhood of Riyadh")
+fig = px.bar(neighborhood_avg_price, x='الحي', y='السعر الاجمالي', 
+             title="Average Price of Villas in Each Neighborhood of Riyadh",
+             labels={'الحي': 'Neighborhood', 'السعر الاجمالي': 'Average Price (SAR)'})
+st.plotly_chart(fig)
+
+# Display average price for each neighborhood
+st.write("Average Price for Each Neighborhood:")
+st.write(neighborhood_avg_price.head(11))
+
